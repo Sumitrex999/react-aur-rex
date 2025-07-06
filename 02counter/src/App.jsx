26835fb2e0b208312475eb794059b8c0e227d26f
Sumit Rex <sumitrex666@gmail.com>
2025-07-06 14:28:45 +0530

    Added a 02counter Project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  //let counter = 12
+  let [counter, setCounter] = useState(12)
+
+  const addValue = () => {
+    setCounter(counter + 1);
+    console.log("clicked", counter + 1);
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+    console.log("clicked", counter - 1);
+  }
+
+
+  return (
+    <>
+      <h1>Rex aur react</h1>
+      <h2>Counter value : {counter} </h2>
+      <button onClick={addValue} >Add value </button>
+      <br />
+      <button onClick= {removeValue} >remove value  </button>
+      {/* <p>Footer: {counter} </p> */}
+      <h1> {counter} </h1>
+
+
+
+      </>
+      
+  );
+}
+
+export default App;
